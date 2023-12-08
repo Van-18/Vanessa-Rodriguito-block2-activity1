fbc846b65dd0307ed203ff975f51a10063441336
Eujill Z. Fabro <changeugene2000@gmail.com>
2023-12-08 11:32:52 +0800

    Testing code

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Vanessa_Rodriguito_Block2_Quiz1"
+include(":app")
+ 
